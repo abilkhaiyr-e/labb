216910ad42fba342b55b81800b62d795986c0abb
abilkhaiyr-e <157514347+abilkhaiyr-e@users.noreply.github.com>
2024-02-01 10:26:00 +0600

    Add files via upload

---
+++ Desktop/labb python/tsis2/lab2/Python Booleans/1.py
@@ -0,0 +1,9 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+"""Output:
+True
+False
+False
+"""
---
+++ Desktop/labb python/tsis2/lab2/Python Booleans/2.py
@@ -0,0 +1,12 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greaer than a")
+
+  """
+  Output:
+  b is not greater than a
+  """
